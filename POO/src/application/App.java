0ac7c72dc401e28b32ad7d8b144eb3d3fe6c1ccb
mateuscesarglima <mateuscesarglima@gmail.com>
2021-12-21 14:42:46 -0300

    Criando novo método para calculo de área

--- POO/src/application/App.java
+++ POO/src/application/App.java
@@ -26,14 +26,11 @@ public class App {
         y.b = sc.nextDouble();
         y.c = sc.nextDouble();
 
-        double p = (x.a + x.b + x.c) / 2.0;
-        double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
+        double areaX = x.area();
+        double areaY = y.area();
 
-        p = (y.a + y.b + y.c) / 2.0;
-        double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) + (p - x.c));
-
-        System.out.println(areaX);
-        System.out.println(areaY);
+        System.out.println("Triangule X area: " + areaX);
+        System.out.println("Triangule Y area: " + areaY);
 
         if(areaX > areaY) {
             System.out.println("Larger area: " + areaX);
--- POO/src/entities/Triangle.java
+++ POO/src/entities/Triangle.java
@@ -7,6 +7,19 @@ public class Triangle {
     public double c;
     
 
+    // Não precisa receber os dados de A,B e C pois já estão na classe.
+    public double area(){
+
+        double p = (a + b +c) / 2.0;
+
+        double result = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        
+        return result;
+        
+    }
+
+
+  
     
 
 }
